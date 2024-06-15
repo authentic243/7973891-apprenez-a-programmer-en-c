162a92de8ceb2a619c771a4e01fdfef947b2aa41
authentic243 <103180599+authentic243@users.noreply.github.com>
2024-06-15 05:30:03 +0200

    je ne code plus dans l'espace que vous aviez predefini avant, mais ca me renvois sur Github

--- P1/P1C4/Livre.cs
+++ P1/P1C4/Livre.cs
@@ -2,7 +2,13 @@
 
 public class Livre
 {
+    Livre monLivre = new Livre ();
+
     public string Titre { get; set; }
     public string Auteur { get; set; }
     public int NombreDePages { get; set; }
+
+    monLivre.Titre = "The almanac";
+    monLivre.Auteur = "Naval Ravikant";
+    monLivre.NombreDePages = 245;
 }
